894fc0b7d7b68d63accb50bfc8bad38e5cbabbee
Uriel098 <a22311064@uthermosiilo.edu.mx>
2024-01-11 10:48:09 -0700

    cree un usuario y agregamos tablas

---
+++ api/uagames/database/migrations/2024_01_11_155545_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("lname");
+            $table->integer("double");
+            $table->integer("phone");
+            $table->string("mail");
+            $table->integer("game_id");
+            $table->integer("console_id");
+            $table->integer("comment_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
